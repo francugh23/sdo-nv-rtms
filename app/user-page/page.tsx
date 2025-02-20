25cd796e7d17745cfd11a078ac1992b82f2ee902
francugh23 <francojohannes23@gmail.com>
2025-02-20 15:32:48 +0800

    wip

--- app/user-page/page.tsx
+++ app/user-page/page.tsx
@@ -1,42 +1,64 @@
 import {
   Card,
   CardContent,
+  CardDescription,
   CardFooter,
   CardHeader,
+  CardTitle,
 } from "@/components/ui/card";
 import Image from "next/image";
 import logo from "@/public/logo.png";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Label } from "@/components/ui/label";
 
 const UserPage = () => {
   return (
-    <div className="w-full max-w-[600px] bg-white rounded-2xl p-8">
-      <div className="max-w-[380px] mx-auto space-y-4">
-        <div className="flex justify-center mb-4">
-          <Image
-            src={logo || "/placeholder.svg"}
-            alt="NVGCHS Logo"
-            width={150}
-            height={150}
-          />
+    <Card className="w-full max-w-[600px] bg-white rounded-2xl p-8">
+      <CardHeader>
+        <CardTitle>Form Submission</CardTitle>
+        <CardDescription>
+          Fill in all the necessary information.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="mx-auto space-y-4">
+        <div className="grid grid-cols-3 gap-4">
+          <div className="col-span-2">
+            <Input type="text" placeholder="Name" />
+          </div>
+          <div className="col-span-1">
+            <Input type="date" placeholder="Date and Time" readOnly/>
+          </div>
+          <div className="col-span-1">
+            <Input type="text" placeholder="School/Agency/District" />
+          </div>
+          <div className="col-span-2">
+            <Input type="text" placeholder="Document Title" />
+          </div>
+          <div className="col-span-1">
+            <Select>
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Type" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Options</SelectLabel>
+                  <SelectItem value="apple">Submit</SelectItem>
+                  <SelectItem value="banana">Apply</SelectItem>
+                  <SelectItem value="blueberry">Receive</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
         </div>
-        <h1 className="text-3xl font-bold mb-0 text-black text-center">
-          Welcome to RTMS!
-        </h1>
-        <p className="text-sm text-gray-600 text-center mb-8">
-          Records Tracking and Management System
-        </p>
-
-        <p className="text-gray-500 text-center">
-          Exit?{" "}
-          <Link href="/login" className="text-blue-500 hover:underline">
-            Log out
-          </Link>
-        </p>
-      </div>
-    </div>
+      </CardContent>
+      <CardFooter className="flex flex-row justify-end space-x-2">
+        <Button variant={"secondary"}>Cancel</Button>
+        <Button variant={"default"}>Submit</Button>
+      </CardFooter>
+    </Card>
   );
 };
 export default UserPage;
